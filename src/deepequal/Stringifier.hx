4a209da1479e8c6647e1dd8d7aa86f537389fc31
Kevin Leung <kevinresol@gmail.com>
2017-04-08 12:50:52 +0800

    Fix neko

--- src/deepequal/Stringifier.hx
+++ src/deepequal/Stringifier.hx
@@ -3,10 +3,10 @@ package deepequal;
 class Stringifier {
 	public static function stringify(v:Dynamic):String {
 		return
-			if(Std.is(v, Date)) DateTools.format(v, '%F %T');
-			else if(Reflect.isEnumValue(v)) Std.string(v);
+			if(Std.is(v, String) || Std.is(v, Float) || Std.is(v, Bool)) haxe.Json.stringify(v);
+			else if(Std.is(v, Date)) DateTools.format(v, '%F %T');
 			else if(Std.is(v, haxe.io.Bytes)) 'bytes(hex):' + (v:haxe.io.Bytes).toHex();
 			else if(haxe.Int64.is(v)) haxe.Int64.toStr(v);
-			else haxe.Json.stringify(v);
+			else Std.string(v);
 	}
 }
